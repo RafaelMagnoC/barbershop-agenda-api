680e687846fe995e5a221d8e65b06ccb8cd1efe2
Rafael Magno <rafaelmagno125@gmail.com>
2024-09-05 19:51:43 -0300

    feat: configuracao do swagger finalizada

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get(`server_port`);
+
+  const config = new DocumentBuilder()
+    .setTitle('Yago Barber Shop')
+    .setDescription('api para gerenciar recursos do sistema Yago Barber Shop')
+    .setVersion('1.0')
+    .addTag('Yago Barber Shop')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: 'api/json',
+  });
   await app.listen(port);
 }
 bootstrap();
