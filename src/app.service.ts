d8bf0e1ec0c148de46e86277bed9c2d151c73163
Rafael Magno <rafaelmagno125@gmail.com>
2024-09-15 10:00:05 -0300

    fix: corrigindo retornando de função getHello

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,13 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const pessoa: any = {
-      id: 1,
-      nome: 'Fernando Magno',
-      idade: 27,
-      genero: 'M',
-    };
-
-    return `O que você está pensando, ${pessoa.nome}`;
+    return 'Hello World!';
   }
 }
